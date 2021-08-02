f5d68b6aeff274b8c65ca6c334857f60bac8db32
Cody Crenshaw <codycrenshaw@Codys-MacBook-Pro.local>
2021-08-02 11:14:41 -0400

    axios added with axios call for pokemon.  Display page component

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class  extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card__title">Charizard</div>
+                <div className="card__image">
+                    <div className="card__stats"></div>
+                </div>
+                
+            </div>
+        )
+    }
+}
